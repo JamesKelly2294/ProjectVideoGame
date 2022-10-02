de26fdd25993366feed8a8595255476054775291
Morgan Cabral <morgan.cabral@gmail.com>
2022-10-02 01:20:30 -0700

    Revert "sorta progress on velocity based movement"
    This reverts commit 3a3ec113db3829abf6182c462f4d9b5b3a18b5d7.

--- Assets/NightOfTheSpook/Combat/Attacker.cs
+++ Assets/NightOfTheSpook/Combat/Attacker.cs
@@ -29,6 +29,8 @@ public class Attacker : MonoBehaviour
     [Range(0.001f, 100)]
     public float DamagePerAttack = 1.0f;
 
+    private Vector3 _movement = Vector3.zero;
+
     private AttackerTasks _currentTask;
     private bool _collidingWithTarget;
 
@@ -67,34 +69,9 @@ public class Attacker : MonoBehaviour
         HandleAttack();
     }
 
-    void FixedUpdate()
-    {
-        var target = PrimaryTarget.GetComponent<Attackable>();
-        if (_currentTask == AttackerTasks.MovingToTarget && target != null && target.IsAlive)
-        {
-            // TODO: Attack props?
-            // TODO: switch to rigid body velocity based movement so we can do physicsy things
-
-            //float magnitude = MovementSpeedInUnitsPerSecond * Time.fixedDeltaTime;
-            //var heading = target.transform.position - transform.position;
-            //heading.y = 0.0f;
-            //var movementVector = magnitude * heading.normalized;
-            //transform.position += (movementVector);
-
-            var rb = GetComponent<Rigidbody>();
-            if (rb.velocity.magnitude < MovementSpeedInUnitsPerSecond)
-            {
-                var heading = target.transform.position - transform.position;
-                heading.y = 0.0f;
-                var velocity = MovementSpeedInUnitsPerSecond * heading.normalized;
-                rb.AddForce(velocity, ForceMode.VelocityChange);
-            }
-        }
-    }
-
     void OnCollisionEnter(Collision collision)
     {
-        var target = PrimaryTarget.GetComponent<Attackable>().gameObject;
+        var target = GetAttackTarget().gameObject;
         var other = collision.gameObject;
         // Debug.Log($"{name} entered collision with ({other.name})");
         if (ReferenceEquals(other, target))
@@ -105,13 +82,18 @@ public class Attacker : MonoBehaviour
 
     void OnCollisionExit(Collision collision)
     {
-        var target = PrimaryTarget.GetComponent<Attackable>().gameObject;
+        var target = GetAttackTarget().gameObject;
         var other = collision.gameObject;
         // Debug.Log($"{name} exited collision with ({other.name})");
         if (ReferenceEquals(other, target))
         {
             _collidingWithTarget = false;
         }
+    }
+
+    private Attackable GetAttackTarget()
+    {
+        return PrimaryTarget != null ? PrimaryTarget.GetComponent<Attackable>() : null;
     }
 
     private string TaskToTaskName(AttackerTasks task)
@@ -131,7 +113,7 @@ public class Attacker : MonoBehaviour
 
     private void UpdateAttackStateMachine()
     {
-        var target = PrimaryTarget.GetComponent<Attackable>();
+        var target = GetAttackTarget();
         if (target == null && _currentTask != AttackerTasks.Idle)
         {
             SwitchToTask(AttackerTasks.Idle, "no target");
@@ -139,7 +121,7 @@ public class Attacker : MonoBehaviour
         }
         else if (target == null)
         {
-            // Handle case where we're already idle to avoid log spew.
+            // Handle case where we're already ideal here to avoid log spew.
             return;
         }
 
@@ -181,14 +163,22 @@ public class Attacker : MonoBehaviour
 
     private void HandleAttack()
     {
-        var target = PrimaryTarget.GetComponent<Attackable>();
+        var target = GetAttackTarget();
         switch (_currentTask)
         {
             case AttackerTasks.Idle:
                 // TODO: move to the random location we previously picked in UpdateMode
+                _movement = Vector3.zero;
                 break;
             case AttackerTasks.MovingToTarget:
-                // Anything to do here?
+                // TODO: Collisions with props
+
+                // TODO: switch to rigid body velocity based movement so we can do physicsy things
+                float magnitude = MovementSpeedInUnitsPerSecond * Time.deltaTime;
+                var heading = target.transform.position - transform.position;
+                heading.y = 0.0f;
+                _movement = magnitude * heading.normalized;
+                transform.position += (_movement);
                 break;
             case AttackerTasks.Attacking:
                 // Perform an attack if we're in range.
