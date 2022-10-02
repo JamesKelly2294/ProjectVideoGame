525bb2d5c24ba1fecfea3a2e80112ec77a2db661
James Kelly <dxk2294@rit.edu>
2022-10-01 22:36:27 -0700

    Get turret working

---
+++ Assets/NightOfTheSpook/Equipment/Content/Static Turret/Projectile.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Projectile : MonoBehaviour
+{
+    [Range(0, 10.0f)]
+    public float speed = 4.0f;
+
+    [Range(0, 10.0f)]
+    public float lifetime = 5.0f;
+
+    [HideInInspector]
+    public float damage;
+
+    private float _startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Impulse);
+
+        _startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > _startTime + lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != LayerConstants.Enemy)
+        {
+            return;
+        }
+
+        var attackable = other.GetComponent<Attackable>();
+        if (attackable == null)
+        {
+            return;
+        }
+
+        attackable.InflictDamage(damage);
+        Destroy(gameObject);
+    }
+}
--- Assets/NightOfTheSpook/Equipment/Content/Static Turret/StaticTurretEquipment.cs
+++ Assets/NightOfTheSpook/Equipment/Content/Static Turret/StaticTurretEquipment.cs
@@ -6,40 +6,93 @@ public class StaticTurretEquipment : Equipment
 {
     public GameObject target;
     public GameObject turretShootyBit;
+    public GameObject projectilePrefab;
+
+    public Transform projectileSpawnPosition;
 
     [Range(0, 20.0f)]
     public float maxAttackRange = 12.0f;
     [Range(0, 20.0f)]
     public float minAttackRange = 3.0f;
 
+    [Range(0, 5.0f)]
+    public float attackCooldown = 1.0f;
+
+    [Range(0, 50.0f)]
+    public float attackDamage = 5.0f;
+
+    [Range(0, 60.0f)]
+    public float lifetime = 30.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
+
+    private float _deltaTime;
 
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
-        {
-            float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
-            if (distanceToTarget > maxAttackRange || distanceToTarget < minAttackRange)
-            {
-                target = null;
-            }
-        }
+        _deltaTime += Time.deltaTime;
 
+        ValidateTarget();
+        AcquireTarget();
+        LookTowardsTarget();
+        FireWeapon();
+    }
+
+    void ValidateTarget()
+    {
         if (target == null)
         {
-            AcquireTarget();
+            return;
         }
 
-        LookTowardsTarget();
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        if (distanceToTarget > maxAttackRange || distanceToTarget < minAttackRange)
+        {
+            target = null;
+            _deltaTime = 0;
+        }
+    }
+
+    void FireWeapon()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        if (_deltaTime < attackCooldown)
+        {
+            return;
+        }
+
+        _deltaTime = 0;
+
+        var projectileGO = Instantiate(projectilePrefab);
+        projectileGO.transform.parent = transform;
+        projectileGO.transform.position = projectileSpawnPosition.position;
+        projectileGO.transform.rotation = turretShootyBit.transform.rotation;
+
+        var projectile = projectileGO.GetComponent<Projectile>();
+        projectile.damage = attackDamage;
+
+        AudioManager.Instance.Play("SFX/TurretFire",
+                pitchMin: 0.9f, pitchMax: 1.1f,
+                volumeMin: 0.5f, volumeMax: 0.5f,
+                position: projectileGO.transform.position,
+                minDistance: 10, maxDistance: 20);
     }
 
     void AcquireTarget()
     {
+        if (target != null)
+        {
+            return;
+        }
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, maxAttackRange, 1 << LayerConstants.Enemy);
         float smallestDistance = Mathf.Infinity;
         foreach (Collider hit in colliders)
@@ -56,17 +109,12 @@ public class StaticTurretEquipment : Equipment
                 }
             }
         }
-
-        if (target != null)
-        {
-            Debug.Log("Turret acquired target = " + target);
-        }
     }
 
     void LookTowardsTarget()
     {
         var damping = 2;
-        var lookPos = target != null ? target.transform.position - turretShootyBit.transform.position : turretShootyBit.transform.position + transform.forward * 10.0f;
+        var lookPos = target != null ? (target.transform.position + Vector3.up) - turretShootyBit.transform.position : turretShootyBit.transform.position + transform.forward * 10.0f;
         var rotation = Quaternion.LookRotation(lookPos);
         turretShootyBit.transform.rotation = Quaternion.Slerp(turretShootyBit.transform.rotation, rotation, Time.deltaTime * damping);
     }
